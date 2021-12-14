4214aed678b95852884c98fb5ac7e7383e19767f
Mike1938 <mmaldonadosan1938@gmail.com>
2021-12-13 21:44:47 -0500

    Created user product invoice and invoice details tables, added Foreign keys to invoice and invoice detail

--- db.sql
+++ db.sql
@@ -23,3 +23,46 @@ CREATE TABLE employee(
     FOREIGN KEY(jobPosition)
     REFERENCES jobPosition(jobCode)
 );
+
+-- User TABLE
+CREATE TABLE user(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fName VARCHAR(45) NOT NULL,
+    lName VARCHAR(45) NOT NULL,
+    companyName VARCHAR(45) NOT NULL,
+    email VARCHAR(100),
+    joined TIMESTAMP NOT NULL DEFAULT NOW(),
+    pwd VARCHAR(100) NOT NULL,
+    phoneNumber VARCHAR(20)
+);
+
+-- Product TABLE
+CREATE TABLE product(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    model VARCHAR(50) NOT NULL,
+    pName VARCHAR(45) NOT NULL,
+    releaseDate year(4) NOT NULL,
+);
+
+-- Invoice TABLE
+CREATE TABLE invoice(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userID INT NOT NULL,
+    employeeID INT NOT NULL,
+    invoiceDate TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY(userID)
+    REFERENCES user(id),
+    FOREIGN KEY(employeeID)
+    REFERENCES employee(id)
+);
+
+-- Invoice Details TABLE
+CREATE TABLE invoiceDetails(
+    equipmentID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    invoiceID INT NOT NULL,
+    productID INT NOT NULL,
+    FOREIGN KEY(invoiceID)
+    REFERENCES invoice(id),
+    FOREIGN KEY(productID)
+    REFERENCES product(id)
+);
