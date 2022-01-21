23b9977b7b0956e7a8d7685bed6439899f047771
Mike1938 <mmaldonadosan1938@gmail.com>
2022-01-20 20:02:54 -0500

    Created the forms for client sign up and worked on the simple validation and insert to the database

--- account/clientSignup.php
+++ account/clientSignup.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Client Signup</title>
-</head>
-<body>
-    <h1>We at the signup page</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Client Signup</title>
+    </head>
+    <body>
+        <h1>New client Registration</h1>
+        <section>
+            <form action="../includes/signupFuncs.php" method="POST">
+                <label for="fName">First Name</label>
+                <input class="styleInput" id="fName" name="fName" type="text" placeholder="First Name">
+                <label for="lName">Last Name</label>
+                <input class="styleInput" id="lName" name="lName" type="text" placeholder="Last Name">
+                <label for="compName">Company Name</label>
+                <input class="styleInput" id="compName" name="compName" type="text" placeholder="Company Name">
+                <label for="email">Email</label>
+                <input class="styleInput" id="email" name="email" type="email" placeholder="Email">
+                <label for="phone">Phone Number</label>
+                <input class="styleInput" id="phone" name="phone" type="text" placeholder="Phone Number">
+                <label for="pwd">PassWord</label>
+                <input class="styleInput" id="pwd" name="pwd" type="password" placeholder="Password">
+                <button name="clientSign">Register</button>
+            </form>
+        </section>
+    </body>
 </html>
--- includes/signupFuncs.php
+++ includes/signupFuncs.php
@@ -1,7 +1,66 @@
 <?php
+
+// ? Connecting to the database
+require_once '../dbConection.php';
+$conn = new mysqli($hn,$un,$pw, $db);
+if($conn->connect_error) die("There was a fatal error");
+
+// TODO make better validations
 if(isset($_POST['clientSign'])){
-    // function to add new client
+
+    $validation = true;
+    // ? Form Variables
+    $fName = $lName = $compName = $email = $pwd = $phone = "";
+
+    // ? Error Variables
+    $fNameErr = $lNameErr = $compNameErr = $emailErr = $pwdErr = $phoneErr = "";
+
+    // ? Checking form variables
+    if(empty($_POST['fName'])){
+        $fNameErr = "Cannot be left blank";
+    }else{
+        $fName = $_POST['fName'];
+    }
+    if(empty($_POST['lName'])){
+        $lNameErr = "Last Name cannot be left blank";
+    }else{
+        $lName = $_POST['lName'];
+    }
+    if(empty($_POST['compName'])){
+        $compNameErr = "Company name cannot be left blank";
+    }
+    else{
+        $compName = $_POST['compName'];
+    }
+    if(empty($_POST['pwd'])){
+        $pwdErr = "Password cannot be left blank";
+    }else{
+        //TODO Code to hash the password
+    }
+    if(empty($_POST['email'])){
+        $emailErr = "Email cannot be left blank";
+    }else{
+        $email = $_POST['email'];
+    }
+    if(empty($_POST['phone'])){
+        $phoneErr = "Phone Number cannot be left blank";
+    }else{
+        $phone = $_POST['phone'];
+    }
+
+    //* Creating the query to insert to the database
+    $query = "INSERT INTO user(fName, lName, companyName, email, pwd, phoneNumber) 
+                Values('$fName', '$lName', '$compName', '$email', '$pwd', '$phone')";
+
+
 }
 elseif(isset($_POST['empSign'])){
     // function to add new employee
 }
+
+if($conn->query($query)){
+    echo "Yay it worked!!";
+}else{
+    echo "There was a problem";
+}
+$conn->close();
