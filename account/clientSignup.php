e053bc74ca69faafa084aedf08fafb55f9346863
Mike1938 <mmaldonadosan1938@gmail.com>
2022-01-22 14:10:58 -0500

    Worked on validation for client signup, and password hash, did function to clen up form data, and worked on sending and displaying errors on client signup site

--- account/clientSignup.php
+++ account/clientSignup.php
@@ -7,20 +7,41 @@
         <title>Client Signup</title>
     </head>
     <body>
+        <?php
+        $fNameErr = $lNameErr = $compNameErr = $emailErr = $phoneErr = $pwdErr = "";
+        if(isset($_GET["fName"])){
+           $fNameErr = $_GET["fName"];
+        }
+        if(isset($_GET["lName"])){
+            $lNameErr = $_GET["lName"];
+        }
+        if(isset($_GET["compName"])){
+           $compNameErr = $_GET["compName"];
+        }
+        if(isset($_GET["email"])){
+           $emailErr = $_GET["email"];
+        }
+        if(isset($_GET["phone"])){
+           $phoneErr = $_GET["phone"];
+        }
+        if(isset($_GET["pwd"])){
+           $pwdErr = $_GET["pwd"];
+        }
+        ?>
         <h1>New client Registration</h1>
         <section>
             <form action="../includes/signupFuncs.php" method="POST">
-                <label for="fName">First Name</label>
+                <label for="fName">First Name <span class="error"><?php echo $fNameErr?></span></label>
                 <input class="styleInput" id="fName" name="fName" type="text" placeholder="First Name">
-                <label for="lName">Last Name</label>
+                <label for="lName">Last Name <span class="error"><?php echo $lNameErr?></span></label>
                 <input class="styleInput" id="lName" name="lName" type="text" placeholder="Last Name">
-                <label for="compName">Company Name</label>
+                <label for="compName">Company Name <span class="error"><?php echo $compNameErr?></span></label>
                 <input class="styleInput" id="compName" name="compName" type="text" placeholder="Company Name">
-                <label for="email">Email</label>
+                <label for="email">Email <span class="error"><?php echo $emailErr?></span></label>
                 <input class="styleInput" id="email" name="email" type="email" placeholder="Email">
-                <label for="phone">Phone Number</label>
+                <label for="phone">Phone Number <span class="error"><?php echo $phoneErr?></span></label>
                 <input class="styleInput" id="phone" name="phone" type="text" placeholder="Phone Number">
-                <label for="pwd">PassWord</label>
+                <label for="pwd">PassWord <span class="error"><?php echo $pwdErr?></span></label>
                 <input class="styleInput" id="pwd" name="pwd" type="password" placeholder="Password">
                 <button name="clientSign">Register</button>
             </form>
--- includes/signupFuncs.php
+++ includes/signupFuncs.php
@@ -4,7 +4,15 @@
 require_once '../dbConection.php';
 $conn = new mysqli($hn,$un,$pw, $db);
 if($conn->connect_error) die("There was a fatal error");
-
+$query= "";
+$urlLocation = "";
+// ? function that cleans the form data
+function test_input($data){
+    $data = trim($data);
+    $data= stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 // TODO make better validations
 if(isset($_POST['clientSign'])){
 
@@ -17,50 +25,128 @@ if(isset($_POST['clientSign'])){
 
     // ? Checking form variables
     if(empty($_POST['fName'])){
-        $fNameErr = "Cannot be left blank";
+        $fNameErr = "Cannot be left blank...";
+        $validation = false;
     }else{
-        $fName = $_POST['fName'];
+        $fName = test_input( $_POST['fName']);
+        if(!preg_match("/^[a-zA-Z ]*$/", $fName)){
+            $fNameErr = 'Only Letters...';
+            $validation = false;
+        }
     }
     if(empty($_POST['lName'])){
-        $lNameErr = "Last Name cannot be left blank";
+        $lNameErr = "Last Name cannot be left blank...";
+        $validation = false;
     }else{
-        $lName = $_POST['lName'];
+        $lName = test_input($_POST['lName']);
+        if(!preg_match("/^[a-zA-Z ]*$/", $lName)){
+            $lNameErr = 'Only Letters...';
+            $validation = false;
+        }
     }
     if(empty($_POST['compName'])){
-        $compNameErr = "Company name cannot be left blank";
+        $compNameErr = "Company name cannot be left blank...";
+        $validation = false;
     }
     else{
-        $compName = $_POST['compName'];
+        $compName = test_input($_POST['compName']);
+        if(strlen($compName) < 3){
+            $validation = "Company Name must be 3 characters or more...";
+        }
     }
     if(empty($_POST['pwd'])){
-        $pwdErr = "Password cannot be left blank";
+        $pwdErr = "Password cannot be left blank...";
+        $validation = false;
     }else{
-        //TODO Code to hash the password
+        $pwd = test_input($pwd);
+        if(strlen($pwd) < 6){
+            $validation = false;
+            $pwdErr = "Password must be 6 characters or more...";
+        }
+        $hashPass = password_hash($pwd, PASSWORD_DEFAULT);
     }
     if(empty($_POST['email'])){
-        $emailErr = "Email cannot be left blank";
+        $emailErr = "Email cannot be left blank...";
+        $validation = false;
     }else{
-        $email = $_POST['email'];
+        $email = test_input($_POST['email']);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $validation = false;
+            $emailErr = 'Email not formatted correctly...';
+        }
     }
     if(empty($_POST['phone'])){
-        $phoneErr = "Phone Number cannot be left blank";
+        $phoneErr = "Phone Number cannot be left blank...";
+        $validation = false;
     }else{
-        $phone = $_POST['phone'];
+        $phone = test_input($phone);
+        if(!preg_match('/^[0-9]*$/', $phone)){
+            $phoneErr = 'Only numbers...';
+            $validation = false;
+        }
     }
-
-    //* Creating the query to insert to the database
-    $query = "INSERT INTO user(fName, lName, companyName, email, pwd, phoneNumber) 
-                Values('$fName', '$lName', '$compName', '$email', '$pwd', '$phone')";
-
-
+    if($validation){
+        //* Creating the query to insert to the database
+        $query = $conn->prepare("INSERT INTO user(fName, lName, companyName, email, pwd, phoneNumber) Values(?,?,?,?,?,?)");
+        $query->bind_param('ssssss',$fName, $lName, $compName, $email, $hashPass, $phone);
+        $urlLocation = "http://localhost/finalProyect/account/clientLogin.php";
+    }else{
+        // TODO Work on sending back the error on validation
+        $goBackUrl = "http://localhost/finalProyect/account/clientSignup.php";
+        $validateGoBack = "http://localhost/finalProyect/account/clientSignup.php";
+        if($fNameErr){
+            $goBackUrl = "{$goBackUrl}?fName={$fNameErr}";
+        }
+        if($lNameErr){
+            if($goBackUrl != $validateGoBack){
+                $goBackUrl = "{$goBackUrl}&lName={$lNameErr}";
+            }else{
+                $goBackUrl = "{$goBackUrl}?lName={$lNameErr}";
+            }
+        }
+        if($compNameErr){
+            if($goBackUrl != $validateGoBack){
+                $goBackUrl = "{$goBackUrl}&compName={$compNameErr}";
+            }else{
+                $goBackUrl = "{$goBackUrl}?compName={$compNameErr}";
+            }        
+        }
+        if($pwdErr){
+            if($goBackUrl != $validateGoBack){
+                $goBackUrl = "{$goBackUrl}&pwd={$pwdErr}";
+            }else{
+                $goBackUrl = "{$goBackUrl}?pwd={$pwdErr}";
+            }
+        }
+        if($emailErr){
+            if($goBackUrl != $validateGoBack){
+                $goBackUrl = "{$goBackUrl}&email={$emailErr}";
+            }else{
+                $goBackUrl = "{$goBackUrl}?email={$emailErr}";
+            }  
+        }
+        if($phoneErr){
+            if($goBackUrl != $validateGoBack){
+                $goBackUrl = "{$goBackUrl}&phone={$phoneErr}";
+            }else{
+                $goBackUrl = "{$goBackUrl}?phone={$phoneErr}";
+            }  
+        }
+        header("location: {$goBackUrl}");
+    }
 }
 elseif(isset($_POST['empSign'])){
     // function to add new employee
 }
+else{
+    header("location: http://localhost/finalProyect/index.php");
+}
 
-if($conn->query($query)){
-    echo "Yay it worked!!";
+// ? Executing the query to the database with the client or employee information
+
+if($query->execute()){
+    header("location: {$urlLocation}?added=Successfully Registered");
 }else{
     echo "There was a problem";
 }
-$conn->close();
+$query->close();
